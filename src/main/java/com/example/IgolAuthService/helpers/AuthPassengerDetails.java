f55b44a5eed86b23660445f5c971ad3cce881a41
Logesh Suresh <logeshsrinivasans@gmail.com>
2024-06-05 17:31:40 +0530

    Feat: Added UserDetails class and UserDetailsServiceImpl given by Spring Security

---
+++ src/main/java/com/example/IgolAuthService/helpers/AuthPassengerDetails.java
@@ -0,0 +1,54 @@
+package com.example.IgolAuthService.helpers;
+
+import com.example.IgolAuthService.models.Passenger;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+// Spring Security works on UserDetails polymorphic type for auth
+
+public class AuthPassengerDetails extends Passenger implements UserDetails {
+
+    private String username;
+
+    private String password;
+
+    public AuthPassengerDetails(Passenger passenger) {
+        this.username = passenger.getEmail();
+        this.password = passenger.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    // Below set of methods are not much of a concern
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
---
+++ src/main/java/com/example/IgolAuthService/services/UserDetailServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.IgolAuthService.services;
+
+import com.example.IgolAuthService.helpers.AuthPassengerDetails;
+import com.example.IgolAuthService.models.Passenger;
+import com.example.IgolAuthService.repositories.PassengerRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/*
+*  This class is responsible for loading the user in the form of UserDetails object for auth
+*/
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final PassengerRepository passengerRepository;
+
+    public UserDetailServiceImpl(PassengerRepository passengerRepository) {
+        this.passengerRepository = passengerRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Passenger> passenger = passengerRepository.findPassengerByEmail(email);
+        if (passenger.isPresent()) {
+            return new AuthPassengerDetails(passenger.get());
+        } else {
+            throw new UsernameNotFoundException("Cannot find the passenger by the EMAIL + " + email);
+        }
+    }
+
+}
